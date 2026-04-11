0168f3db8bb2bffe6a1db3072baf510d3e5f80ab
Shawn <shawn@mccool.email>
2026-04-12 00:45:08 +0200

    feat(settings): hide editable forms behind change links

--- test/scry_2_web/live/settings_live_test.exs
+++ test/scry_2_web/live/settings_live_test.exs
@@ -13,17 +13,22 @@ defmodule Scry2Web.SettingsLiveTest do
 
       {:ok, view, _html} = live(conn, ~p"/settings")
 
+      open_edit(view, "player_log_path")
+
       view
       |> form("form[phx-submit='save_player_log_path']", value: path)
       |> render_submit()
 
       assert Settings.get("mtga_logs_player_log_path") == path
+      refute render(view) =~ "form[phx-submit='save_player_log_path']"
     end
 
     test "shows an error for an invalid path", %{conn: conn, tmp_dir: tmp_dir} do
       missing = Path.join(tmp_dir, "nope.log")
 
       {:ok, view, _html} = live(conn, ~p"/settings")
+
+      open_edit(view, "player_log_path")
 
       html =
         view
@@ -33,11 +38,26 @@ defmodule Scry2Web.SettingsLiveTest do
       assert html =~ "No file exists"
       refute Settings.get("mtga_logs_player_log_path") == missing
     end
+
+    test "cancel hides the form without saving", %{conn: conn} do
+      {:ok, view, _html} = live(conn, ~p"/settings")
+
+      open_edit(view, "player_log_path")
+      assert render(view) =~ "save_player_log_path"
+
+      view
+      |> element("button[phx-click='cancel_edit'][phx-value-field='player_log_path']")
+      |> render_click()
+
+      refute render(view) =~ "save_player_log_path"
+    end
   end
 
   describe "data_dir form" do
     test "saves a valid directory", %{conn: conn, tmp_dir: tmp_dir} do
       {:ok, view, _html} = live(conn, ~p"/settings")
+
+      open_edit(view, "data_dir")
 
       view
       |> form("form[phx-submit='save_data_dir']", value: tmp_dir)
@@ -50,6 +70,8 @@ defmodule Scry2Web.SettingsLiveTest do
       missing = Path.join(tmp_dir, "nope")
 
       {:ok, view, _html} = live(conn, ~p"/settings")
+
+      open_edit(view, "data_dir")
 
       html =
         view
@@ -98,6 +120,8 @@ defmodule Scry2Web.SettingsLiveTest do
     test "saves a valid cron expression", %{conn: conn} do
       {:ok, view, _html} = live(conn, ~p"/settings")
 
+      open_edit(view, "refresh_cron")
+
       view
       |> form("form[phx-submit='save_refresh_cron']", value: "0 5 * * *")
       |> render_submit()
@@ -108,6 +132,8 @@ defmodule Scry2Web.SettingsLiveTest do
     test "shows an error for an invalid expression", %{conn: conn} do
       {:ok, view, _html} = live(conn, ~p"/settings")
 
+      open_edit(view, "refresh_cron")
+
       html =
         view
         |> form("form[phx-submit='save_refresh_cron']", value: "not a cron")
@@ -115,5 +141,11 @@ defmodule Scry2Web.SettingsLiveTest do
 
       assert html =~ "Invalid cron"
     end
+  end
+
+  defp open_edit(view, field) do
+    view
+    |> element("button[phx-click='start_edit'][phx-value-field='#{field}']")
+    |> render_click()
   end
 end
