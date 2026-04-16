b8a98556eeff97f495740c41fb484a0f9e0725b4
Shawn <shawn@mccool.email>
2026-04-16 09:41:41 +0200

    chore: release v0.12.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Scry2.MixProject do
   def project do
     [
       app: :scry_2,
-      version: "0.11.1",
+      version: "0.12.0",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
